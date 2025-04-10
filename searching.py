96296a30a916de4a35b2c384f78cb9862e6567b0
Adam Havlik <269696@vutbr.cz>
2025-04-10 09:29:44 +0200

    ukol 2

--- searching.py
+++ searching.py
@@ -1,22 +1,39 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
 
 
 def read_data(file_name, field):
-    """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
+
+    if field not in{"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        None
+
+    with open(file_name, "r") as file_obj:
+        data=json.load(file_obj)
+        s=data[field]
+        return s
+
+
     file_path = os.path.join(cwd_path, file_name)
 
 
-def main():
-    pass
+def linear_search(sekvence, cislo):
+    pozice=[]
+    count=0
+    for index, hodnota in enumerate(sekvence):
+        if hodnota == cislo:
+            pozice.append(index)
+            count += 1
+    return {"positions":pozice,
+            "count": count
+    }
 
+def main():
+    sequential_data=read_data("sequential.json","unordered_numbers")
+    print(sequential_data)
+    print(linear_search(sequential_data,9))
 
 if __name__ == '__main__':
     main()
